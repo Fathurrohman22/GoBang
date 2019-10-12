fb8cba2133bf9d69490fff6c3d6817e657c83c38
Fathurrohman <fathur22101998@gmail.com>
2019-10-12 01:54:46 -0700

    Menambahkan form pembayaran

--- app/src/main/java/fathurrohman/cv/gobang/MainActivity.java
+++ app/src/main/java/fathurrohman/cv/gobang/MainActivity.java
@@ -1,18 +1,14 @@
 package fathurrohman.cv.gobang;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -23,7 +19,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        loadFragment(new Cari());
+        loadFragment(new Beranda());
         btHome = findViewById(R.id.btHome);
         btFavorite = findViewById(R.id.btFavorite);
         btAccount = findViewById(R.id.btAccount);
--- app/src/main/java/fathurrohman/cv/gobang/ValidasiData.java
+++ app/src/main/java/fathurrohman/cv/gobang/ValidasiData.java
@@ -1,14 +1,28 @@
 package fathurrohman.cv.gobang;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class ValidasiData extends AppCompatActivity {
+
+    Button lanjut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_validasi_data);
+
+        lanjut = findViewById(R.id.btnLanjut);
+        lanjut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent lanjut = new Intent(ValidasiData.this, Pembayaran.class);
+                startActivity(lanjut);
+            }
+        });
     }
 }
